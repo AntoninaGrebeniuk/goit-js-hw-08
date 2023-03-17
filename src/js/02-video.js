2f900d1fea7928bcd558a39d23bd1fc85b0ab553
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-03-17 23:48:13 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -26,3 +26,38 @@
 // воспроизведение с сохраненной позиции.
 //* Добавь в проект библиотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в
 //* хранилище не чаще чем раз в секунду.
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(CURRENT_TIME);
+
+// iframe.addEventListener('timeupdate', throttle(onPlay, 1000));
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
