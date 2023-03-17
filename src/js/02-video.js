9a4db4dd672ddde07a700758422bf3208bbf375a
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-03-18 00:10:34 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,19 +14,18 @@ const onPlay = function (data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player
-  .setCurrentTime(savedTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+player.setCurrentTime(savedTime);
+// .then(function (seconds) {
+//   // seconds = the actual time that the player seeked to
+// })
+// .catch(function (error) {
+//   switch (error.name) {
+//     case 'RangeError':
+//       // the time was less than 0 or greater than the video’s duration
+//       break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+//     default:
+//       // some other error occurred
+//       break;
+//   }
+// });
